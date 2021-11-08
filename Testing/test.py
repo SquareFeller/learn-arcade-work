b18a7fffe5f1e9236a4395500953b674f6614a90
Isai RIncon <ifrincon@ucsc.edu>
2021-11-08 12:14:21 -0800

    Sketti

---
+++ Testing/test.py
@@ -0,0 +1,13 @@
+print("It was a dark and stormy night.")
+print("~~~~~"*4)
+print("According to all known laws of aviation,")
+print(" ")
+print("You thought this was going to an error \" But AHA the backslash has struck again")
+print("The file is stored in C: \\new folder")
+print("This\nis\nmy\nsample\nof\none\nstring\non\nseveral\nlines")
+print(" ")
+print("""Buenos dias,
+amigos.
+Estoy en una otra linea.
+Jeje.
+Es porque estoy usando triple quotes.""")
