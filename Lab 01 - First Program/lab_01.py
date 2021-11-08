04f5ffe014b50778edd374edf7a5cd914cef6567
Isai RIncon <ifrincon@ucsc.edu>
2021-11-08 12:26:42 -0800

    Changes to Lab 1 file

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,11 @@
-print("Howdy, camarones")
+print("Howdy, fellas.\nHow are we doing at this fine saloon")
+print(" ")
+print("I'm right here!\nNevermind, I'm here now!")
+print(" ")
+print("""Printing is pretty cool. 
+I'm very curious to learn more.
+ESPECIALLY when it comes to game development.""")
+print(" ")
+print("""A duck walked up to the lemonade stand and he said to the man running the stand:
+"Hey, got any grapes?"
+:-)""")
